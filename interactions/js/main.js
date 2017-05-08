e3a65238e150e5ffe0ad1f966eeac2636891f8d1
ShivamBh <shivam.bhattacharjee94@gmail.com>
2017-05-08 15:25:58 +0530

    js modified

--- interactions/js/main.js
+++ interactions/js/main.js
@@ -1,3 +1,33 @@
 (function() {
-    alert("works");
+
+    window.addEventListener("wheel", function(e) {
+        
+
+        
+        var dir = checkScrollDir(e)
+        if (dir === -1) {
+            console.log("Up");
+        }
+
+        if (dir === 1) {
+            console.log("Down");
+        }
+
+
+        return false
+    });
+
+
+    // #func check scroll direction
+    function checkScrollDir(e) {
+        if (e.deltaY > 0) {
+            return 1;
+        }
+
+        if (e.deltaY < 0) {
+            return -1;
+        }
+    }
+
+
 })();
