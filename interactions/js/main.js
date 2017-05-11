719b79b9b08f1cd1f515a2eed7f0d74503d89f92
ShivamBh <shivam.bhattacharjee94@gmail.com>
2017-05-11 16:46:28 +0530

    mousewheel func mods

--- interactions/js/main.js
+++ interactions/js/main.js
@@ -3,27 +3,56 @@
     var arr_slides = document.querySelectorAll(".slide");
     arr_length = arr_slides.length;
     //console.log(arr_length)
-    first = arr_slides[0];
+    
+    var curr = 0;
+    var next = 1;
+    var prev = -1;
 
-    console.log(classie.hasClass(first, "is-active"));
+    
 
     window.addEventListener("wheel", function(e) {
-        
-
         
         var dir = checkScrollDir(e)
         if (dir === -1) {
             console.log("Up");
+            var_prev_slide = get_prev_slide(curr);
+
         }
 
         else {
             console.log("Down");
-            
+            var next_slide = get_next_slide(curr);
+            console.log(next_slide);
+            console.log(classie.has(next_slide, "is-active"));
         }
 
 
         return false
     });
+
+    //get previous elem from index param
+    function get_previous_slide(curr_index) {
+        classie.remove(arr_slides[curr_index], "is-active");
+        classie.add(arr_slides[curr_index+1], "is-active");
+        if (curr_index === 0) {
+            return false
+        }
+        curr -= 1;
+        next -= 1;
+    }
+
+    //func get current elem from index param
+    function get_next_slide(curr_index) {
+        if (curr_index === arr_length) {
+            return false;
+        }
+        classie.remove(arr_slides[curr_index], "is-active");
+        classie.add(arr_slides[curr_index+1], "is-active");
+
+        curr += 1;
+        next += 1; 
+        return arr_slides[curr_index+1];
+    }
 
 
     // #func check scroll direction
