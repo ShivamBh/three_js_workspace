7d2df4c481d9b8eb0276df2ec39496f71c8aef00
ShivamBh <shivam.bhattacharjee94@gmail.com>
2017-05-13 12:38:32 +0530

    isogram.js O(n2)

--- codewars/isogram.js
+++ codewars/isogram.js
@@ -1,1 +1,17 @@
-console.log("yo");
+function isIsogram(str) {
+
+	var ss = str.toLowerCase().split("");
+	var char = ss[0];
+	for(var i=0; i < ss.length; i++) {
+		for(var j=i+1; j < ss.length; j++) {
+			if(ss[i] === ss[j]) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+
+}
+
+console.log(isIsogram("Dermatoglyphicss"));
